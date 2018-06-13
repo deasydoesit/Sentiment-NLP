cb1158649dc0e816d6a62370d1bd40ef4afce594
deasydoesit <darwin.rinderer@gmail.com>
2018-06-13 13:12:58 -0400

    Creating crypto function

--- app/routing/apiRoute.js
+++ app/routing/apiRoute.js
@@ -1,11 +1,13 @@
 var twitterSearch = require("../../app/modules/twitter.js");
-var watsonNlp = require("../../app/modules/watson.js")
+var watsonNlp = require("../../app/modules/watson.js");
+var cryptoSearch = require("../../app/modules/crypto.js");
 
 module.exports = function(app) {
 
     app.get("/api", function(req, res) {
         twitterSearch();
         watsonNlp();
+        cryptoSearch("ETH");
     });
 
 };
