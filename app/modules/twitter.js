3b11901f9df08d347e63c1c3637ba374bf939bc4
SOIP <38543876+sumofitsparts@users.noreply.github.com>
2018-06-13 13:22:37 -0400

    twitter api working

--- app/modules/twitter.js
+++ app/modules/twitter.js
@@ -1,8 +1,36 @@
 require("dotenv").config();
 
+var express = require("express");
+
 var keys = require("../data/keys/keys");
 var Twitter = require('twitter');
 var client = new Twitter(keys.twitter);
+var path = require("path");
+
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+var hello = "hello"
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+
+app.get("/api/query", function(req, res) {
+    return res.json(hello);
+  });
+
+  app.post("/api/query", function(req, res) {
+    
+    var search = req.body;
+
+    console.log(search);
+  
+    hello.push(search);
+  
+    res.json(search);
+  });
 
 var twitterSearch = function() {
 
@@ -15,4 +43,21 @@ var twitterSearch = function() {
      
 };
 
-module.exports = twitterSearch;
+var tag = "trump";
+
+var client = new Twitter({
+  consumer_key: 'kS1uEtsZfGIsBh4XKO88yT0cg',
+  consumer_secret: '8ExXQ7wdGQ3qYu53a8hyHzbVExBMJkl8SvXbJXn1DXQ23mCVlS',
+  access_token_key: '1004547933612724224-5DryC0uN3fV7IP1at6mNOWb8ywegWI',
+  access_token_secret: 'KCsZR4HlCBg8sSQaMVmTRxBE8cNDb7yzyEnQhG4aXp4zM'
+});
+ 
+client.get('search/tweets', {q: tag}, function(error, tweets, response) {
+    for (i=0; i <= 10; i++) {
+    console.log(tweets.statuses[i].text);
+    }
+ });
+
+ app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
