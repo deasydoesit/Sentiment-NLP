63347b917f4eb7903cf25fdab9774d80282ae690
abhilashalk <abhilulk@gmail.com>
2018-06-15 14:46:22 -0400

    file changes

--- app/routing/apiRoute.js
+++ app/routing/apiRoute.js
@@ -1,4 +1,4 @@
-require("dotenv").config(); 
+require("dotenv").config();
 
 var keys = require("../data/keys/keys");
 var axios = require("axios");
@@ -11,18 +11,18 @@ var getPrice = require("../../app/modules/crypto.js");
 var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
 var natural_language_understanding = new NaturalLanguageUnderstandingV1(keys.watson);
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-    app.post("/api", function(req, res) {
+    app.post("/api", function (req, res) {
 
         var results = {};
 
-        axios.all([getTweets(req.body.search), getPrice()])
-            .then(axios.spread(function(tweets, price) {
+        axios.all([getTweets(req.body.search), getPrice(req.body.search)])
+            .then(axios.spread(function (tweets, price) {
 
                 // price handling 
-                var avgPriceArray = price.data.Data.reduce(function(accum, current) {
-                    var avg = ((current.high + current.low ) / 2);
+                var avgPriceArray = price.data.Data.reduce(function (accum, current) {
+                    var avg = ((current.high + current.low) / 2);
                     accum.push(Number.parseFloat(avg).toFixed(2));
                     return accum
                 }, []);
@@ -37,32 +37,32 @@ module.exports = function(app) {
                 return text;
 
             }))
-            .then(function(text){
+            .then(function (text) {
                 var parameters = {
                     'text': 'IBM is an American multinational technology company headquartered in Armonk, New York, United States, with operations in over 170 countries.',
                     'features': {
-                      'entities': {
-                        'emotion': true,
-                        'sentiment': true,
-                        'limit': 2
-                      },
-                      'keywords': {
-                        'emotion': true,
-                        'sentiment': true,
-                        'limit': 2
-                      }
+                        'entities': {
+                            'emotion': true,
+                            'sentiment': true,
+                            'limit': 2
+                        },
+                        'keywords': {
+                            'emotion': true,
+                            'sentiment': true,
+                            'limit': 2
+                        }
                     }
-                  }
-                  
-                  natural_language_understanding.analyze(parameters, function(err, response) {
+                }
+
+                natural_language_understanding.analyze(parameters, function (err, response) {
                     if (err)
                         console.log('error:', err);
                     else {
                         results.watson = response
                         return res.send(results);
-                  }});
+                    }
+                });
             });
     });
-    
+
 };
- 
