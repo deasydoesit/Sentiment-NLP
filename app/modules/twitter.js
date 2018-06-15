f79b0a4c3bb0ed0e2044c1f6545dd29e67a45064
deasydoesit <darwin.rinderer@gmail.com>
2018-06-15 00:15:57 -0400

    Creating second processing pipeline implementation with data return to client

--- app/modules/twitter.js
+++ app/modules/twitter.js
@@ -1,24 +1,21 @@
 require("dotenv").config(); 
-
-var watsonNlp = require("./watson.js");
-
+var axios = require("axios");
 var keys = require("../data/keys/keys");
-var Twitter = require('twitter');
-var client = new Twitter(keys.twitter);
 
-var twitterSearch = function(search) {
-    var textToBeAnalyzed = [];
-    client.get('search/tweets', {q: search, result_type: "popular", tweet_mode: "extended"})
-          .then(function(tweets) {
-                for (var i = 0; i < tweets.statuses.length; i++) {
-                    textToBeAnalyzed.push(tweets.statuses[i].full_text);
-                }
-                var text = textToBeAnalyzed.join();
-                watsonNlp(text);
-          })
-          .catch(function(error){
-              throw error;
-          });
+function getTweets(search) {
+    return  axios({
+        method: "get",
+        baseURL: "https://api.twitter.com/1.1/search/tweets.json",
+        headers: {
+            "Authorization": "Bearer " + keys.twitter.bearer_token
+        },
+        params: {
+            q: search,
+            result_type: "popular", 
+            tweet_mode: "extended",
+            count: 9
+        }
+    });
 };
 
-module.exports = twitterSearch;
+module.exports = getTweets;
--- app/routing/apiRoute.js
+++ app/routing/apiRoute.js
@@ -1,23 +1,68 @@
 require("dotenv").config(); 
 
-var twitterSearch = require("../../app/modules/twitter.js");
-var watsonNlp = require("../../app/modules/watson.js");
-var cryptoSearch = require("../../app/modules/crypto.js");
+var keys = require("../data/keys/keys");
+var axios = require("axios");
+
+var keys = require("../data/keys/keys.js")
+var getTweets = require("../../app/modules/twitter.js");
+var getPrice = require("../../app/modules/crypto.js");
+
+
+var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
+var natural_language_understanding = new NaturalLanguageUnderstandingV1(keys.watson);
 
 module.exports = function(app) {
 
-    //Not important, jst for testing
-    // app.get("/api", function(req, res) {
-    //     twitterSearch();
-    //     watsonNlp();
-    //     cryptoSearch("ETH");
-    // });
-   
     app.post("/api", function(req, res) {
-        var userSearch = req.body.search;
-        twitterSearch(userSearch);
-        res.json(req.body);
-    });
 
+        var results = {};
+
+        axios.all([getTweets(req.body.search), getPrice()])
+            .then(axios.spread(function(tweets, price) {
+
+                // price handling 
+                var avgPriceArray = price.data.Data.reduce(function(accum, current) {
+                    var avg = ((current.high + current.low ) / 2);
+                    accum.push(Number.parseFloat(avg).toFixed(2));
+                    return accum
+                }, []);
+                results.price = avgPriceArray;
+
+                //tweets handling
+                var textToBeAnalyzed = [];
+                for (var i = 0; i < tweets.data.statuses.length; i++) {
+                    textToBeAnalyzed.push(tweets.data.statuses[i].full_text);
+                }
+                var text = textToBeAnalyzed.join();
+                return text;
+
+            }))
+            .then(function(text){
+                var parameters = {
+                    'text': 'IBM is an American multinational technology company headquartered in Armonk, New York, United States, with operations in over 170 countries.',
+                    'features': {
+                      'entities': {
+                        'emotion': true,
+                        'sentiment': true,
+                        'limit': 2
+                      },
+                      'keywords': {
+                        'emotion': true,
+                        'sentiment': true,
+                        'limit': 2
+                      }
+                    }
+                  }
+                  
+                  natural_language_understanding.analyze(parameters, function(err, response) {
+                    if (err)
+                        console.log('error:', err);
+                    else {
+                        results.watson = response
+                        return res.send(results);
+                  }});
+            });
+    });
+    
 };
  
