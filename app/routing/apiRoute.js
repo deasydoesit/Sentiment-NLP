6b6fbcc68698f05a320bbacc93c1f7494d20c11f
deasydoesit <darwin.rinderer@gmail.com>
2018-06-16 17:21:58 -0400

    Improving user search to account for both coin name and ticker

--- app/routing/apiRoute.js
+++ app/routing/apiRoute.js
@@ -2,9 +2,10 @@ require("dotenv").config();
 
 var axios = require("axios");
 
-var keys = require("../data/keys/keys");
+var keys = require("../data/keys/keys.js");
 var getTweets = require("../../app/modules/twitter.js");
 var getPrice = require("../../app/modules/crypto.js");
+var coins = require("../data/coins.js")
 
 
 var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
@@ -14,11 +15,17 @@ module.exports = function (app) {
 
     app.post("/api", function (req, res) {
 
+        function isCoin(coin) {
+            return coin.includes(req.body.search.toLowerCase());
+        }
+
+        var userSearch = coins.find(isCoin);
+    
         var results = {};
 
-        axios.all([getTweets(req.body.search), getPrice(req.body.search)])
+        axios.all([getTweets(userSearch[0]), getPrice(userSearch[1].toUpperCase())])
             .then(axios.spread(function(tweets, price) {
-
+                
                 // price handling 
                 var avgPriceArray = price.data.Data.reduce(function (accum, current) {
                     var avg = ((current.high + current.low) / 2);
@@ -33,35 +40,36 @@ module.exports = function (app) {
                     textToBeAnalyzed.push(tweets.data.statuses[i].full_text);
                 }
                 var text = textToBeAnalyzed.join();
+                console.log(text, avgPriceArray);
                 return text;
 
             }))
-            .then(function (text) {
-                var parameters = {
-                    'text': 'IBM is an American multinational technology company headquartered in Armonk, New York, United States, with operations in over 170 countries.',
-                    'features': {
-                        'entities': {
-                            'emotion': true,
-                            'sentiment': true,
-                            'limit': 2
-                        },
-                        'keywords': {
-                            'emotion': true,
-                            'sentiment': true,
-                            'limit': 2
-                        }
-                    }
-                }
+            // .then(function (text) {
+            //     var parameters = {
+            //         'text': 'IBM is an American multinational technology company headquartered in Armonk, New York, United States, with operations in over 170 countries.',
+            //         'features': {
+            //             'entities': {
+            //                 'emotion': true,
+            //                 'sentiment': true,
+            //                 'limit': 2
+            //             },
+            //             'keywords': {
+            //                 'emotion': true,
+            //                 'sentiment': true,
+            //                 'limit': 2
+            //             }
+            //         }
+            //     }
 
-                natural_language_understanding.analyze(parameters, function (err, response) {
-                    if (err)
-                        console.log('error:', err);
-                    else {
-                        results.watson = response
-                        return res.send(results);
-                    }
-                });
-            });
+            //     natural_language_understanding.analyze(parameters, function (err, response) {
+            //         if (err)
+            //             console.log('error:', err);
+            //         else {
+            //             results.watson = response
+            //             return res.send(results);
+            //         }
+            //     });
+            // });
     });
 
 };
