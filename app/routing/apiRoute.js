adab8ef20d406c4618b0831c035561773edab457
deasydoesit <darwin.rinderer@gmail.com>
2018-06-18 10:05:51 -0400

    Refactoring backend

--- app/routing/apiRoute.js
+++ app/routing/apiRoute.js
@@ -15,14 +15,18 @@ module.exports = function (app) {
 
     app.post("/api", function (req, res) {
 
+        //results obj, ultimately returned to client device w/ price and sentiment data
+        var results = {};
+
+        //function to identify coin name/ticker array, corresponding to user search, for api query purposes
         function isCoin(coin) {
             return coin.includes(req.body.search.toLowerCase());
         }
 
+        //userSearch captures the identified coin name/ticker array, corresponding to user search, for api query purposes
         var userSearch = coins.find(isCoin);
-    
-        var results = {};
 
+        //concurrent requests to Twitter API to get tweets and CryptoCompar API to get price associated with user searched coin
         axios.all([getTweets(userSearch[0]), getPrice(userSearch[1].toUpperCase())])
             .then(axios.spread(function(tweets, price) {
                 
@@ -40,11 +44,12 @@ module.exports = function (app) {
                     textToBeAnalyzed.push(tweets.data.statuses[i].full_text);
                 }
                 var text = textToBeAnalyzed.join();
-                console.log(text, avgPriceArray);
                 return text;
 
             }))
             .then(function (text) {
+
+                //watson config parameters
                 var parameters = {
                     'text': text,
                     'features': {
@@ -52,6 +57,7 @@ module.exports = function (app) {
                         },
                 }
 
+                //watson api call, returns results obj to client device
                 natural_language_understanding.analyze(parameters, function (err, response) {
                     if (err)
                         console.log('error:', err);
